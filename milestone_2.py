97442799052e3adda1998c14157741f710ca00c3
RTFRekola <rtfrekola@gmail.com>
2023-02-10 18:25:00 +0000

    Checks for input validity

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,19 @@
 import random
 
+# Create a list of five fruits.
 word_list = ["papaya", "mango", "watermelon", "pear", "apple"]
 print(word_list)
 
+# Choose randomly one of the fruits.
 word = random.choice(word_list)
 print(word)
 
+# Ask user to guess one letter."
 guess = input("Please, give a single letter: ")
+
+# Check if the guess is indeed a letter and just one letter.
+if (guess.isalpha() and len(guess)==1):
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+# end if
