292c1f18423ad7cc8e2c1c3fe964b58c007d8528
RTFRekola <rtfrekola@gmail.com>
2023-02-16 17:21:58 +0000

    Creates methods for running the tasks

--- milestone_4.py
+++ milestone_4.py
@@ -12,31 +12,36 @@ class Hangman:
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
-# Check if the approved letter is in the randomly chosen word. 
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print("Good guess!", guess, "is in the word.")
-    else: 
-        print("Sorry,", guess, "is not in the word. Try again.")
-    # end if
-    return
-
-# Ask for user input and check it is valid.
-def ask_for_input():
-    while True:
-        # Ask user to guess one letter."
-        guess = input("Please, give a single letter: ")
-
-        # Check if the guess is indeed a letter and just one letter.
-        if (guess.isalpha() and len(guess)==1):
-            break
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
+    def check_guess(self, guess):
+        '''
+        This method checks if the approved letter is in the randomly chosen word. 
+        '''
+        guess = guess.lower()
+        if guess in word:
+            print("Good guess!", guess, "is in the word.")
+        #else: 
+        #    print("Sorry,", guess, "is not in the word. Try again.")
         # end if
-    # end while
-    check_guess(guess)
-    return
+        return
+
+    def ask_for_input(self):
+        '''
+        This method asks for user input and checks that it is valid.
+        '''
+        while True:
+            # Ask user to guess one letter."
+            guess = input("Please, give a single letter: ")
+            
+            # Check if the guess is indeed a letter and just one letter.
+            if not (guess.isalpha() and len(guess)==1):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                check_guess(guess)
+            # end if
+        # end while
+        return
 
 # Create a list of five fruits.
 word_list = ["papaya", "mango", "watermelon", "pear", "apple"]
